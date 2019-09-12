608c2d6c46826cfd39edfa44968248cc2bc1085d
steven nguyen <sqnguyen829@gmail.com>
2019-09-12 22:31:15 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,26 +1,33 @@
 # Your Code Here
 
-def map(s)
-  new = []
-  i = 0
-  while i < s.length
-    new.push(yield(s[i]))
-    i += 1
+def map(start_array)
+  new_array=[]
+  i=0 
+  while i < start_array.length do
+    #new_array << start_array[i] * 2 
+    new_array << (yield (start_array[i] ))
+    i += 1 
   end
-  new
-end
+  new_array
+end 
 
-def reduce(s, sp=nil)
-  if sp
-    accum = sp
+
+
+def reduce(start_array, array_total=nil )
+  
+  if array_total
+    new_total = array_total
     i = 0
+    
   else
-    accum = s[0]
+    new_total = start_array[0]
     i = 1
   end
-  while i < s.length
-    accum = yield(accum, s[i])
+  
+  while i < start_array.length
+    new_total = yield(new_total, start_array[i])
     i += 1
   end
-  accum
-end
+  
+  new_total
+end 
